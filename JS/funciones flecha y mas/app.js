a126fc10530aa658489aa0303189992dd359030e
jhosB <jabarrios001@misena.edu.co>
2021-10-09 20:54:40 -0500

    commit para codigo js

--- JS/funciones flecha y mas/app.js
+++ JS/funciones flecha y mas/app.js
@@ -1,1 +1,75 @@
+//unstruccion usada para mensaje en la pagina
 document.write("<h1>que se dice</h1>");
+
+// *** TIPOS DE DATO ***
+
+//string="cadena de caracteres"
+
+//numerico= 11150 or  -1 or 1,50190
+
+//Tipo de dato logico
+//boolean= true or false 
+
+
+//lista de items = ARRAY
+// ["pipe","sandra","zoe"] esto es un areglo array
+// [1,5,6,87,9]
+// [true,false,true,false]
+
+
+// object 
+
+// ({
+//     "username":"alfonso",
+//     "score":70.4,
+//     "hours":14,
+//     "proffesional":true,
+//     "friends":["pipe","juanita","xl107xl"]
+// })
+
+
+// *** VARIABLES ***
+
+//variable para poder hacer cambios como editar nombres de usuario o contraseña etc..
+
+var nameUser = "anita";
+
+let lastNamee = "del barrio";
+
+//Reasignando el valor de la variable nameUser
+
+var nameUser = "lisape";
+
+
+//console.log(nameUser,lastName);
+
+//su valor(unico) no se puede alterar ya que se mantiene constante 
+
+const pepe="pepe";
+
+
+// *** OPERADORES ***
+
+let numberOne= 70;
+let numberTwo= 70;
+
+let resultado = numberOne + numberTwo;
+let resultado2 = numberOne < numberTwo;
+
+// > <  != == <=
+
+console.log(resultado);
+console.log(resultado2);
+
+// + - * /
+
+//String
+
+let namee= "chinita";
+let lastName= "de la china";
+
+// CONCATENAZION 
+
+let resultadoo = namee + " " + lastName;
+
+console.log(resultadoo);
